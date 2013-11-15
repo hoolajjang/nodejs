bb5df3476f0f59e687743e8f1457348f4e4c65d7
Kim Kwanghee <hoolajjang@gmail.com>
2013-11-14 20:42:53 -0500

    C9 test

--- run.js
+++ run.js
@@ -1,11 +1,6 @@
-
 var http = require("http");
 
 http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type':'text/plain', 'Access-Control-Allow-Origin':'*'});
 	res.end(req.connection.remoteAddress + ":" + req.connection.remotePort);
-}).listen("8080", "192.18.18.102");
-
-
-
-
+}).listen(process.env.PORT, process.env.IP);
