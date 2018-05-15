f5b2a97a542da290fe627f4d5c702cbb9059b20b
hoolajjang <hoolajjang@gmail.com>
2018-05-15 23:37:21 +0900

    update basic source

--- run.js
+++ run.js
@@ -3,4 +3,6 @@ var http = require("http");
 http.createServer(function(req, res) {
 	res.writeHead(200, {'Content-Type':'text/plain', 'Access-Control-Allow-Origin':'*'});
 	res.end(req.connection.remoteAddress + ":" + req.connection.remotePort);
-}).listen(process.env.PORT, process.env.IP);
+}).listen(1818, "127.0.0.1");
+
+console.log('Server running at http://127.0.0.1:1818/');
